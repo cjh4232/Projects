fa95107cf55c4899936eae0439923e0bd979d0eb
cjh4232 <cody.hatch24@gmail.com>
2022-08-28 11:53:50 -0400

    Added docstrings & updated format for PEP 8

--- Python/HTML_Document_Reader/html_doc_reader.py
+++ Python/HTML_Document_Reader/html_doc_reader.py
@@ -4,115 +4,135 @@ import pandas as pd
 import nltk
 #nltk.download()
 
-class HTML_Doc_Reader():
-    
-    # This class was created to load data acquired from a Trioptics
-    # ImageMaster and output into an html document
-    # The data of interest was both the sagittal & tangential MTF values 
-    # at 200 lp/mm
- 
-    # The class requires a root directory where the data is stored to be
-    # instantiated
+class HTMLDocReader():
+
+    """
+    This code creates a custom class that allows users to convert complicated output
+    documents (html) from the Trioptics ImageMaster, which are frankly garbage, into
+    a usable form for data analysis.
+
+    The class is instantiated using the directory in which the data is being stored.
+
+    The get_data() function is called on the new object and a Pandas DataFrame is returned
+
+    In this version the data of interest was both the sagittal & tangential  MTF 
+    values @ 200 lp/mm
+    """
+
     def __init__(self, root_dir) -> None:
         self.root_dir = root_dir
-    
-    # The get_list function acquires a list of all the files in the root
-    # directory with the file type of "html" and returns that list
-    def get_list(self) -> List:
-        
-        # Begin with full list of files & directories in root
+
+    def __get_list(self) -> List:
+
+        """
+        This function acquires a list of all the files in the root directory with the file type
+        "html" and returns that list
+        """
+        # Create a full list of files & directories in root directory
         full_list = os.listdir(self.root_dir)
-        
+
         # Create empty list to store upcoming html file names
         html_list = []
-        
+
         # Check the last 4 indexes for the "html" file type
         for file in full_list:
             if file[-4:] == "html":
                 html_list.append(file) # Add them to the list
-        
+
         return html_list
 
-    # The get_SN function assumes the html file was saved with a specific naming
-    # nomenclature and the SN is the first set of numbers in the filename
-    def get_SN(self, fileName) -> str:
-        if fileName[1] == "_":
-            return fileName[0]
-        else:
-            return fileName[0:2]
+    def __get_sn(self, file_name) -> str:
+        """
+        This function assumes the html file was saved with a specific naming convention
+        and the SN is the first set of numbers in the filename and returns that SN
+        """
+        if file_name[1] == "_":
+            return file_name[0]
 
-    # The get_loc function assumes the html file was saved with a standards naming
-    # nomenclature and the location is within the file name
-    def get_loc(self, fileName) -> str:
-        if fileName[-6] == "C":
-            return fileName[-6]
-        else:
-            return fileName[-7:-5]
-    
-    # The read_html_NLTK function uses the Natural Language Tool Kit to parse the html document,
-    # find the lp/mm value of interest (in this case, 200) and then retrieve the data points
-    # associated with that value of interest by going into the nested lists of document information
-    def read_html_NLTK(self, fileName, freq):
-        
+        return file_name[0:2]
+
+    def __get_loc(self, file_name) -> str:
+        """
+        This function assumes the html file was saved with a specific naming convention
+        and the location is within the filename
+        """
+        if file_name[-6] == "C":
+            return file_name[-6]
+        return file_name[-7:-5]
+
+    def __read_html_nltk(self, file_name, freq):
+        """
+        This function uses the Natural Language Tool Kit to parse the html document, find the lp/mm
+        value of interest (in this case, 200) and then retrieve the data points associated with that
+        value of interest by going into the nested lists of document information and return a list
+        """
+
         # Opens the file using the root directory provided in the class instaiation & file name
-        file = open(self.root_dir + fileName, 'r')
-        
+        file = open(self.root_dir + file_name, 'r', encoding='utf-8')
+
         # Reads file in variable
         read_file = file.read()
-        
+
         # Creates a text object to store the document
         text = nltk.Text(nltk.word_tokenize(read_file))
-        
+
         # Obtain a list of matches to the value of interest
         match = text.concordance_list(freq)
-        
+
         # Create an empty list to store upcoming data
         data = []
-        
-        # Iterate through each matching search item and obtain the corresponding 
+
+        # Iterate through each matching search item and obtain the corresponding
         # measurement value within the nested lists and convert to a float
         for line in match:
             data.append(float(line[2][14]))
-        
+
         return data
-    
-    # The get_tan function uses the read_html_NLTK function to get the two datapoints
-    # out of the document and returns the tangential value
-    def get_tan(self, fileName, freq) -> float:
-        data = self.read_html_NLTK(fileName, freq)
+
+    def __get_tan(self, file_name, freq) -> float:
+        """
+        This function uses the __read_html_nltk fucntion to get the two datapoints out
+        of the document and returns the tangential value
+        """
+        data = self.__read_html_nltk(file_name, freq)
         return data[1]
 
-    # The get_sag function uses the read_html_NLTK function to get the two datapoints
-    # out of the document and returns the saggital value
-    def get_sag(self, fileName, freq) -> float:
-        data = self.read_html_NLTK(fileName, freq)
+    def __get_sag(self, file_name, freq) -> float:
+        """
+        This function uses the __read_html_nltk fucntion to get the two datapoints out
+        of the document and returns the saggital value
+        """
+        data = self.__read_html_nltk(file_name, freq)
         return data[0]
-    
-    # The get_data function makes use of all of the built-in class functions to build
-    # mulitple lists of data and returns them zipped together into a pandas DataFrame
+
     def get_data(self, freq):
-        
+        """
+        This function uses all of the built-in class functions to build multiple lists of
+        data and returns them zipped together into a Pandas DataFrame.
+
+        This is the only function that should be called using the HTML_Document_Reader class.
+        """
         # Create empty lists to store upcoming data
-        SN = []
-        LOC = []
-        SAG = []
-        TAN = []
+        serial_numbers = []
+        locations = []
+        saggital_values = []
+        tangential_values = []
         
         # Create a list of column names - the order matters
-        COL_NAMES = ["SN", "Location", "Sagittal", "Tangential"]
+        column_names = ["SN", "Location", "Sagittal", "Tangential"]
         
         # Creates list of files in root directory of html file type
-        file_list = self.get_list()
+        file_list = self.__get_list()
 
         # Iterate through each file and add all of the information from the file to 
         # the corresponding list using the class functions
         for file in file_list:
-            SN.append(self.get_SN(file))
-            LOC.append(self.get_loc(file))
-            SAG.append(self.get_sag(file, freq))
-            TAN.append(self.get_tan(file, freq))
+            serial_numbers.append(self.__get_sn(file))
+            locations.append(self.__get_loc(file))
+            saggital_values.append(self.__get_sag(file, freq))
+            tangential_values.append(self.__get_tan(file, freq))
         
         # Create a pandas DataFrame from the zipped lists and column names
-        df = pd.DataFrame(zip(SN, LOC, SAG, TAN), columns=COL_NAMES)
+        dataframe = pd.DataFrame(zip(serial_numbers, locations, saggital_values, tangential_values), columns=column_names)
         
-        return df
+        return dataframe
